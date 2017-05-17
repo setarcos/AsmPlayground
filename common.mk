8f8c1e1cb5d6a32edc2dae75c235d133d51842d6
YanJun Yang <yangyj.ee@gmail.com>
2017-05-17 13:54:39 +0800

    New common.mk for vvfat emulation

--- common.mk
+++ common.mk
@@ -14,7 +14,7 @@ else
 	wlink format dos option map name ../$(OUTDIR)/$(PROG).exe file $(PROG).obj file ../libs/mylib.lib
 endif # USELIB
 emu:
-	qemu-system-x86_64 ../../dos.cow -hdb fat:../$(OUTDIR) $(QEMUFLAG)
+	qemu-system-x86_64 ../../dos.cow -drive file=fat:rw:../$(OUTDIR),format=raw $(QEMUFLAG)
 endif # FLAT
 $(PROG).obj: $(PROG).asm
 	jwasm -omf -Zi -Fo $(PROG).obj $(PROG).asm
